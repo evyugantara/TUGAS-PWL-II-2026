d5a3fc76fa60ea706ac8fa71b67b9accded0567a
Ega Virga Yugantara <egavirga01@email.com>
2026-04-26 00:32:29 +0700

    TUGAS PWL II SEDER/MIGRATION/ERD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\ContacController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\RiwayatController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/About', [AboutController::class, 'index']);
+Route::get('/Beranda', [BerandaController::class, 'index']);
+Route::get('/Contac', [ContacController::class, 'index']);
+Route::get('/Produk', [ProdukController::class, 'index']);
+Route::get('/Riwayat', [RiwayatController::class, 'index']);
